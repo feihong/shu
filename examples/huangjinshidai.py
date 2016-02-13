31797351bae4480d721eab213d4d683f453c0e5f
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-13 17:05:14 -0600

    Add huangjinshidai example

---
+++ examples/huangjinshidai.py
@@ -0,0 +1,13 @@
+from shu import KanunuScraper
+
+
+class MyBookScraper(KanunuScraper):
+    index_url = 'http://www.kanunu8.com/book3/7065/index.html'
+    title = '黄金时代'
+    author = '王小波'
+
+
+if __name__ == '__main__':
+    scraper = MyBookScraper('books/huangjin shidai')
+    scraper.download()
+    scraper.build_ebook('huangjin shidai.txt')
