7b81cb9f9e8f71009d8bdf63f94ac8d9470c58e5
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-13 17:05:06 -0600

    Refactor some existing examples to use KanunuScraper

--- examples/guichuideng.py
+++ examples/guichuideng.py
@@ -1,35 +1,13 @@
-from urlpath import URL
-from shu import BookScraper, Node, print_node
+from shu import KanunuScraper
 
 
-class MyBookScraper(BookScraper):
+class MyBookScraper(KanunuScraper):
     index_url = 'http://www.kanunu8.com/files/yuanchuang/200806/627.html'
     title = '鬼吹灯（盗墓者的经历）'
     author = '本物天下霸唱'
-    base_url = URL(index_url).parent
-
-    def get_title_and_links(self, doc):
-        anchors = doc("td a[href ^= '627']")
-        for anchor in anchors:
-            yield (
-                anchor.text_content(),
-                str(self.base_url / anchor.get('href')))
-
-    def get_links(self, doc):
-        for title, link in self.get_title_and_links(doc):
-            yield link
-
-    def get_content_tree(self, doc):
-        root = Node(title='root')
-        for title, link in self.get_title_and_links(doc):
-            doc = self.get_doc(link)
-            chapter = Node(title=title)
-            chapter.content = doc('p')[0].text_content()
-            root.append(chapter)
-        return root
 
 
 if __name__ == '__main__':
-    scraper = MyBookScraper('books/guichuideng')
+    scraper = MyBookScraper('books/gui chui deng')
     scraper.download()
-    scraper.build_ebook('guichuideng.txt')
+    scraper.build_ebook('gui chui deng.txt')
---
+++ examples/qiuzhuangshandian.py
@@ -0,0 +1,13 @@
+from shu import KanunuScraper
+
+
+class MyBookScraper(KanunuScraper):
+    index_url = 'http://www.kanunu8.com/book3/6633/index.html'
+    title = '球状闪电'
+    author = '刘慈欣'
+
+
+if __name__ == '__main__':
+    scraper = MyBookScraper('books/qiu zhuang shan dian')
+    scraper.download()
+    scraper.build_ebook('qiu zhuang shan dian.txt')
