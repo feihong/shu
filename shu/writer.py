6c5eeb00836ae996e78ab2b06688c7b20fe880cc
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-13 18:03:49 -0600

    Add basic paging functionality

--- shu/writer.py
+++ shu/writer.py
@@ -1,11 +1,24 @@
 class PagedFileWriter:
+    """
+    File-like writer that adds page markers at specific intervals.
+
+    """
+    limit = 700
+
     def __init__(self, filename):
         self.fp = None
         self.filename = filename
-        self.buffer = ''
+        self.page = 1
 
     def write(self, text):
-        self.fp.write(text)
+        if text.startswith('#'):
+            self.fp.write(text)
+            return
+
+        for chunk in self._get_chunks(text):
+            self.fp.write(chunk)
+            self.fp.write('\n\n-%d-\n\n' % self.page)
+            self.page += 1
 
     def __enter__(self):
         self.fp = open(self.filename, 'w')
@@ -13,3 +26,12 @@ class PagedFileWriter:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.fp.close()
+
+    def _get_chunks(self, text):
+        if len(text) < self.limit:
+            yield text
+            raise StopIteration
+
+        while len(text):
+            yield text[:self.limit]
+            text = text[self.limit:]
