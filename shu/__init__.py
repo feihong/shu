8f0c33baf1fc7a491c1566b689d537c3059ab878
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-14 19:55:02 -0600

    Add make_ebook() utility function

--- shu/__init__.py
+++ shu/__init__.py
@@ -13,3 +13,9 @@ def print_node(node):
         print('%s%s' % (spaces, node))
         for child in reversed(node.children):
             stack.append((level+1, child))
+
+
+def make_ebook(scraper_class, output_file, add_page_markers=True):
+    scraper = scraper_class()
+    scraper.download()
+    scraper.build_ebook(output_file, add_page_markers=add_page_markers)
