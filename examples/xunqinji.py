0a903535f9af48c0372d485248158e4bbe7f2eb4
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-14 19:38:26 -0600

    Refactor xunqinji to use the new KanunuMultiVolumeScraper class

--- examples/xunqinji.py
+++ examples/xunqinji.py
@@ -1,42 +1,15 @@
-from urlpath import URL
-from shu import KanunuScraper, Node
+from shu import KanunuMultiVolumeScraper
 
 
-class MyBookScraper(KanunuScraper):
+class MyBookScraper(KanunuMultiVolumeScraper):
     index_url = 'http://www.kanunu8.com/wuxia/201103/2336.html'
     title = '寻秦记'
     author = '黄易'
-    index_table_selector = 'table[cellpadding="7"] a'
-
-    def get_content_tree(self, doc):
-        root = Node(title='root')
-
-        # The table that contains the scroll and chapter titles.
-        table = doc('table[cellpadding="7"]')
-
-        for td in table('td'):
-            # Ignore empty cells.
-            if not td.getchildren():
-                continue
-
-            if td.find('strong') is not None:
-                root.append(Node(title=td.text_content()))
-            else:
-                scroll = root.lastchild.title
-                title = '%s: %s' % (scroll, td.text_content())
-                chapter = Node(title=title)
-
-                url = self.base_url / td.find('a').get('href')
-                doc = self.get_doc(url)
-                chapter.content = doc('p')[0].text_content()
-
-                root.lastchild.append(chapter)
-
-        return root
+    index_table_selector = 'table[cellpadding="7"]'
 
 
 if __name__ == '__main__':
-    # Quite long, has 290 chapters.
+    # 290 chapters, oveeer 2500 pages.
     scraper = MyBookScraper('cache')
     scraper.download()
-    scraper.build_ebook('xun qin ji.txt', add_page_markers=True)
+    scraper.build_ebook('xun qin ji.txt')
